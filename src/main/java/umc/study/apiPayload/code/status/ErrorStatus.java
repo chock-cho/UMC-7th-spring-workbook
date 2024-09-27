76e4f1dd29c26d64cadd67c8e6cdbc35f70d23c6
jamey <kimjunhwan@gimjunhwan-ui-MacBookPro.local>
2024-09-27 19:03:39 +0900

    feat: week 9 워크북 실습 완료

--- src/main/java/umc/study/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/umc/study/apiPayload/code/status/ErrorStatus.java
@@ -26,6 +26,9 @@ public enum ErrorStatus implements BaseErrorCode {
     // FoodCategory Error
     FOOD_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "FOOD_CATEGORY4001", "음식 카테고리가 없습니다."),
 
+    // Store Error
+    STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE_4001","가게가 없습니다."),
+
     // Article Error
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
 
